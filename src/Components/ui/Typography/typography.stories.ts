d9f021a12972f020aec9a6ce9055a893e406fb9c
Gennadij Lobotskij <utyf185@mail.ru>
2023-11-13 20:20:49 +0300

    Add typographic

---
+++ src/Components/ui/Typography/typography.stories.ts
@@ -0,0 +1,106 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Typography } from './'
+
+const meta = {
+  argTypes: {
+    variant: {
+      control: { type: 'radio' },
+      options: [
+        'Large',
+        'H1',
+        'H2',
+        'H3',
+        'Body1',
+        'Subtitle1',
+        'Body2',
+        'Subtitle2',
+        'Caption',
+        'Overline',
+        'Link1',
+        'Link2',
+      ],
+    },
+  },
+  component: Typography,
+  tags: ['autodocs'],
+  title: 'Components/Typography',
+} satisfies Meta<typeof Typography>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const child = 'Componenta'
+
+export const Body1: Story = {
+  args: {
+    children: child,
+    variant: 'Body1',
+  },
+}
+export const Body2: Story = {
+  args: {
+    children: child,
+    variant: 'Body2',
+  },
+}
+export const Caption: Story = {
+  args: {
+    children: child,
+    variant: 'Caption',
+  },
+}
+export const H1: Story = {
+  args: {
+    children: child,
+    variant: 'H1',
+  },
+}
+export const H2: Story = {
+  args: {
+    children: child,
+    variant: 'H2',
+  },
+}
+export const H3: Story = {
+  args: {
+    children: child,
+    variant: 'H3',
+  },
+}
+export const Large: Story = {
+  args: {
+    children: child,
+    variant: 'Large',
+  },
+}
+export const Link1: Story = {
+  args: {
+    children: child,
+    variant: 'Link1',
+  },
+}
+export const Link2: Story = {
+  args: {
+    children: child,
+    variant: 'Link2',
+  },
+}
+export const Overline: Story = {
+  args: {
+    children: child,
+    variant: 'Overline',
+  },
+}
+export const Subtitle1: Story = {
+  args: {
+    children: child,
+    variant: 'Subtitle1',
+  },
+}
+export const Subtitle2: Story = {
+  args: {
+    children: child,
+    variant: 'Subtitle2',
+  },
+}
---
+++ src/Components/ui/Typography/typography.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './typography.module.scss'
+
+const COMPONENTS = {
+  Body1: 'p',
+  Body2: 'p',
+  Caption: 'span',
+  H1: 'h1',
+  H2: 'h2',
+  H3: 'h3',
+  Large: 'span',
+  Link1: 'a',
+  Link2: 'a',
+  Overline: 'p',
+  Subtitle1: 'p',
+  Subtitle2: 'p',
+  error: 'span',
+} as const
+
+type Component = keyof typeof COMPONENTS
+
+export type TypographyProps<T extends ElementType> = {
+  children?: ReactNode
+  className?: string
+  component?: T
+  variant?: Component
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+) => {
+  const { basicName, className, component, variant = 'Body1', ...rest } = props
+  const Component = component || COMPONENTS[variant] || 'span'
+  const classNames = clsx(s[variant], className)
+
+  return <Component className={classNames} {...rest} />
+}
+
+// export const Typography = {
+//   Body1: CreateTypography({ variant: 'Body1' }),
+//   Body2: CreateTypography({ variant: 'Body2' }),
+//   Caption: CreateTypography({ variant: 'Caption' }),
+//   H1: CreateTypography({ variant: 'H1' }),
+//   H2: CreateTypography({ variant: 'H2' }),
+//   H3: CreateTypography({ variant: 'H3' }),
+//   Large: CreateTypography({ variant: 'Large' }),
+//   Link1: CreateTypography({ variant: 'Link1' }),
+//   Link2: CreateTypography({ variant: 'Link2' }),
+//   Overline: CreateTypography({ variant: 'Overline' }),
+//   Subtitle1: CreateTypography({ variant: 'Subtitle1' }),
+//   Subtitle2: CreateTypography({ variant: 'Subtitle2' }),
+// }
