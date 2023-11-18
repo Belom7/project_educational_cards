1ace3b6f35ed07a96b9b2e55ddc051dd6800d5bd
Gennadij Lobotskij <utyf185@mail.ru>
2023-11-18 23:39:33 +0300

    Adding header

--- src/components/assets/icons/index.ts
+++ src/components/assets/icons/index.ts
@@ -1,3 +1,4 @@
 export { default as Check } from './check'
 export { default as Eye } from './eye'
 export { default as EyeOutline } from './eye-outline'
+export { default as Logo } from './logoItIncubator'
