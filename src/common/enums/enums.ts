6a79fb865b2a20107ae5689b8a3fa2c508f15034
Richard Leshik <richardleshik@gmail.com>
2023-11-20 22:51:43 +0300

    feat: add enums

---
+++ src/common/enums/enums.ts
@@ -0,0 +1,22 @@
+export enum TypographyOption {
+  Body1 = 'p',
+  Body2 = 'p',
+  Caption = 'span',
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  Large = 'span',
+  Link1 = 'a',
+  Link2 = 'a',
+  Overline = 'p',
+  Subtitle1 = 'p',
+  Subtitle2 = 'p',
+  error = 'span',
+}
+
+export enum ButtonOption {
+  Link = 'link',
+  Primary = 'primary',
+  Secondary = 'secondary',
+  Tertiary = 'tertiary',
+}
