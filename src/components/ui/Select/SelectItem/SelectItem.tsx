0672888ae663134a8763694c058e0ff75046de3c
Richard Leshik <richardleshik@gmail.com>
2023-11-28 20:57:57 +0300

    feat: update SelectItem subcomponent

--- src/components/ui/Select/SelectItem/SelectItem.tsx
+++ src/components/ui/Select/SelectItem/SelectItem.tsx
@@ -1,17 +1,30 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
+import { TypographyOption } from '@/common/enums'
+import { SelectOption } from '@/components/ui/Select'
+import { Typography } from '@/components/ui/Typography'
 import * as SelectRadix from '@radix-ui/react-select'
+import { clsx } from 'clsx'
+
+import s from '../Select.module.scss'
 
 export type SelectItemProps = {
-  className: string
+  className?: string
+  option?: SelectOption
   value: string
 } & ComponentPropsWithoutRef<typeof SelectRadix.Root>
 
 export const SelectItem = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectItemProps>(
-  ({ children, className, ...restProps }, ref): JSX.Element => {
+  ({ children, className, option = 'default', ...restProps }, ref): JSX.Element => {
+    const typographyOption = option === 'default' ? TypographyOption.Body1 : TypographyOption.Body2
+
+    const classNames = clsx(s[`${option}Paddings`], s.selectItem, className)
+
     return (
-      <SelectRadix.Item className={className} {...restProps} ref={ref}>
-        <SelectRadix.ItemText>{children}</SelectRadix.ItemText>
+      <SelectRadix.Item className={classNames} {...restProps} ref={ref}>
+        <SelectRadix.ItemText>
+          <Typography variant={typographyOption}>{children}</Typography>
+        </SelectRadix.ItemText>
       </SelectRadix.Item>
     )
   }
