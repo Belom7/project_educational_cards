3de057694a04d6497ebf80e52c0818e68f99c9df
Richard Leshik <richardleshik@gmail.com>
2023-11-29 21:44:16 +0300

    feat: add Select component stories

--- src/components/ui/Select/Select.stories.tsx
+++ src/components/ui/Select/Select.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Select } from './Select'
+import { Select, SelectItemArgs } from './Select'
 
 const meta: Meta<typeof Select> = {
   component: Select,
@@ -11,9 +11,37 @@ const meta: Meta<typeof Select> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+const defaultSelectItems: SelectItemArgs[] = [
+  { child: 'Select-box', value: 'Select-box1' },
+  { child: 'Select-box', value: 'Select-box2' },
+  { child: 'Select-box', value: 'Select-box3' },
+]
+
 export const Default: Story = {
   args: {
     className: 'default',
-    value: 'Radix Select',
+    disabled: false,
+    placeholder: defaultSelectItems[0].child,
+    selectItems: defaultSelectItems,
+    title: 'Select-box',
+    variant: 'default',
+  },
+}
+
+const paginationSelectItems: SelectItemArgs[] = [
+  { child: '10', value: '10' },
+  { child: '25', value: '25' },
+  { child: '50', value: '50' },
+  { child: '100', value: '100' },
+]
+
+export const Pagination: Story = {
+  args: {
+    className: 'pagination',
+    disabled: false,
+    placeholder: paginationSelectItems[3].child,
+    selectItems: paginationSelectItems,
+    title: 'Select-box',
+    variant: 'pagination',
   },
 }
