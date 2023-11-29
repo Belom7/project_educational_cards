a0319f8ae8b99129b8beea42954aa59414ce7625
Richard Leshik <richardleshik@gmail.com>
2023-11-29 21:42:27 +0300

    refactor: update SelectItem subcomponent

--- src/components/ui/Select/SelectItem/SelectItem.tsx
+++ src/components/ui/Select/SelectItem/SelectItem.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import { TypographyOption } from '@/common/enums'
+import { SelectOption, SelectVariant } from '@/components/ui/Select'
 import { Typography } from '@/components/ui/Typography'
 import * as SelectRadix from '@radix-ui/react-select'
 import { clsx } from 'clsx'
@@ -9,13 +10,17 @@ import s from '../Select.module.scss'
 
 export type SelectItemProps = {
   className?: string
-  option?: TypographyOption.Body1 | TypographyOption.Body2
+  option?: SelectOption
   value: string
+  variant: SelectVariant
 } & ComponentPropsWithoutRef<typeof SelectRadix.Root>
 
 export const SelectItem = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectItemProps>(
-  ({ children, className, option = TypographyOption.Body1, ...restProps }, ref): JSX.Element => {
-    const classNames = clsx(s[`${option}Paddings`], s.selectItem, className)
+  (
+    { children, className, option = TypographyOption.Body1, variant, ...restProps },
+    ref
+  ): JSX.Element => {
+    const classNames = clsx(s[`${variant}Paddings`], s.selectItem, className)
 
     return (
       <SelectRadix.Item className={classNames} {...restProps} ref={ref}>
