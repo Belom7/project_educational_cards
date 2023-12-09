b96c9962af4e4fd9556da55945c67086a136e2c9
Gennadij Lobotskij <utyf185@mail.ru>
2023-12-09 13:38:59 +0300

    Fix: Header button

--- src/components/ui/Header/Header.tsx
+++ src/components/ui/Header/Header.tsx
@@ -1,4 +1,6 @@
-import { TypographyOption } from '@/common/enums'
+import { Link } from 'react-router-dom'
+
+import { Routes, TypographyOption } from '@/common/enums'
 import { Logo } from '@/components/assets'
 import { Button } from '@/components/ui/Button'
 import { Typography } from '@/components/ui/Typography'
@@ -16,7 +18,9 @@ export const Header = ({ authorized = false, avatar, name = 'Ivan' }: HeaderProp
         <Logo />
       </div>
       {!authorized ? (
-        <Button className={s.other}>Sing In</Button>
+        <Button as={Link} className={s.other} to={Routes.Login}>
+          Sing In
+        </Button>
       ) : (
         <div className={s.other}>
           <Typography className={s.name} variant={TypographyOption.Subtitle1}>
