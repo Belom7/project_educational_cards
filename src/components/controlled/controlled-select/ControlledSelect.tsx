9966076699d54ac5332d659770c0fb30a9ae5fd8
Richard Leshik <richardleshik@gmail.com>
2023-12-13 22:15:12 +0300

    feat: add ControlledSelect component

---
+++ src/components/controlled/controlled-select/ControlledSelect.tsx
@@ -0,0 +1,19 @@
+import { JSX } from 'react'
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Select, SelectProps } from '@/components/ui/Select'
+
+type ControlledSelectProps<T extends FieldValues> = UseControllerProps<T> &
+  Omit<SelectProps, 'onValueChange' | 'value'>
+
+export const ControlledSelect = <T extends FieldValues>({
+  control,
+  name,
+  ...restProps
+}: ControlledSelectProps<T>): JSX.Element => {
+  const {
+    field: { onChange, value },
+  } = useController({ control, name })
+
+  return <Select {...restProps} onValueChange={onChange} value={value} />
+}
