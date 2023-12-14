e37830f30922c9be2b366bf8d77413d3364157a7
Belom7 <Still07pkm@gmail.com>
2023-12-14 23:43:24 +0300

    fix: fix style

--- src/components/assets/icons/componentsFromIcon/PersonOutline.tsx
+++ src/components/assets/icons/componentsFromIcon/PersonOutline.tsx
@@ -1,17 +1,11 @@
 import { SVGProps } from 'react'
 export const PersonOutline = (props: SVGProps<SVGSVGElement>) => (
-  <svg fill={'none'} height={24} width={24} xmlns={'http://www.w3.org/2000/svg'} {...props}>
-    <g clipPath={'url(#a)'} fill={'currentColor'}>
-      <path
-        d={
-          'M12 11a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm0-6a2 2 0 1 1 0 4 2 2 0 0 1 0-4Zm0 8a7 7 0 0 0-7 7 1 1 0 1 0 2 0 5 5 0 1 1 10 0 1 1 0 0 0 2 0 7 7 0 0 0-7-7Z'
-        }
-      />
-    </g>
-    <defs>
-      <clipPath id={'a'}>
-        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
-      </clipPath>
-    </defs>
+  <svg {...props} fill={'currentColor'} height={16} width={16} xmlns={'http://www.w3.org/2000/svg'}>
+    <path
+      d={
+        'M8 7.33A2.67 2.67 0 1 0 8 2a2.67 2.67 0 0 0 0 5.33Zm0-4A1.33 1.33 0 1 1 8 6a1.33 1.33 0 0 1 0-2.67Zm0 5.34a4.67 4.67 0 0 0-4.67 4.66.67.67 0 1 0 1.34 0 3.33 3.33 0 0 1 6.66 0 .67.67 0 0 0 1.34 0A4.67 4.67 0 0 0 8 8.67Z'
+      }
+      fill={'currentColor'}
+    />
   </svg>
 )
