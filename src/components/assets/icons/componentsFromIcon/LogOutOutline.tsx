5e7074a22d1d17ec0e2e029b1dd71217cbf27293
Belom7 <Still07pkm@gmail.com>
2023-12-14 23:43:16 +0300

    fix: fix style

--- src/components/assets/icons/componentsFromIcon/LogOutOutline.tsx
+++ src/components/assets/icons/componentsFromIcon/LogOutOutline.tsx
@@ -1,17 +1,11 @@
 import { SVGProps } from 'react'
 export const LogOutOutline = (props: SVGProps<SVGSVGElement>) => (
-  <svg fill={'none'} height={24} width={24} xmlns={'http://www.w3.org/2000/svg'} {...props}>
-    <g clipPath={'url(#a)'} fill={'currentColor'}>
-      <path
-        d={
-          'M7 6a1 1 0 0 0 0-2H5a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2H6V6h1Zm13.8 5.4-2.8-4a1 1 0 1 0-1.6 1.2L18 11h-8a1 1 0 0 0 0 2h8l-1.8 2.4a1 1 0 0 0 1.6 1.2l3-4a1 1 0 0 0 0-1.2Z'
-        }
-      />
-    </g>
-    <defs>
-      <clipPath id={'a'}>
-        <path d={'M0 0h24v24H0z'} fill={'#fff'} />
-      </clipPath>
-    </defs>
+  <svg {...props} fill={'none'} height={16} width={16} xmlns={'http://www.w3.org/2000/svg'}>
+    <path
+      d={
+        'M4.7 4a.7.7 0 1 0 0-1.3H3.3a.7.7 0 0 0-.6.6v9.4a.7.7 0 0 0 .6.6h1.4a.7.7 0 1 0 0-1.3H4V4h.7Zm9.2 3.6L12 5a.7.7 0 1 0-1 .8l1 1.6H6.8a.7.7 0 0 0 0 1.4H12l-1.2 1.6a.7.7 0 0 0 .1.9.7.7 0 0 0 1-.1l2-2.7a.7.7 0 0 0 0-.8Z'
+      }
+      fill={'#fff'}
+    />
   </svg>
 )
