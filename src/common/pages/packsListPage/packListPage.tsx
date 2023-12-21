4443e04f9b8369c0fd4bb0305a3e1c1e4b8bd1ff
Gennadij Lobotskij <utyf185@mail.ru>
2023-12-21 20:40:09 +0300

    Fix:Header

--- src/common/pages/packsListPage/packListPage.tsx
+++ src/common/pages/packsListPage/packListPage.tsx
@@ -1,9 +1,23 @@
+import { TypographyOption } from '@/common/enums'
+import { Button } from '@/components/ui/Button'
 import { Header } from '@/components/ui/Header'
+import { Typography } from '@/components/ui/Typography'
 
+import s from './packListPage.module.scss'
 export const PackListPage = () => {
   return (
     <>
       <Header authorized></Header>
+      <div className={s.main}>
+        <div className={s.wrapper}>
+          <div className={s.packsList}>
+            <Typography variant={TypographyOption.H1}>Packs list</Typography>
+            <Button>
+              <Typography variant={TypographyOption.Subtitle2}>Add New Pack</Typography>
+            </Button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
