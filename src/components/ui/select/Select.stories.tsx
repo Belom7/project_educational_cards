b17c64a0d5fdc41537a1ab9c662248b275499497
Richard Leshik <richardleshik@gmail.com>
2024-02-03 19:09:45 +0300

    fix: add /select from Git issue

---
+++ src/components/ui/select/Select.stories.tsx
@@ -0,0 +1,72 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Select, SelectItemArgs } from './Select'
+
+const meta: Meta<typeof Select> = {
+  component: Select,
+  tags: ['autodocs'],
+  title: 'Components/Select',
+} satisfies Meta<typeof Select>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+const defaultSelectItems: SelectItemArgs[] = [
+  { child: 'Select-box1', value: 'Select-box1' },
+  { child: 'Select-box2', value: 'Select-box2' },
+  { child: 'Select-box3', value: 'Select-box3' },
+]
+
+export const Default: Story = {
+  args: {
+    className: 'default',
+    disabled: false,
+    fullWidth: false,
+    placeholder: 'Select value',
+    selectItems: defaultSelectItems,
+    title: 'Select-box',
+    variant: 'default',
+  },
+}
+
+export const fullWidth: Story = {
+  args: {
+    className: 'default',
+    disabled: false,
+    fullWidth: true,
+    placeholder: 'Select value',
+    selectItems: defaultSelectItems,
+    title: 'Select-box',
+    variant: 'default',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    className: 'default',
+    disabled: true,
+    fullWidth: false,
+    placeholder: 'Select value',
+    selectItems: defaultSelectItems,
+    title: 'Select-box',
+    variant: 'default',
+  },
+}
+
+const paginationSelectItems: SelectItemArgs[] = [
+  { child: '10', value: '10' },
+  { child: '25', value: '25' },
+  { child: '50', value: '50' },
+  { child: '100', value: '100' },
+]
+
+export const Pagination: Story = {
+  args: {
+    className: 'pagination',
+    disabled: false,
+    placeholder: paginationSelectItems[3].child,
+    selectItems: paginationSelectItems,
+    title: 'Select-box',
+    variant: 'pagination',
+  },
+}
---
+++ src/components/ui/select/select-item/SelectItem.tsx
@@ -0,0 +1,33 @@
+import { ComponentPropsWithoutRef, ElementRef, JSX, forwardRef } from 'react'
+
+import { TypographyOption } from '@/common/enums'
+import { Typography } from '@/components'
+import { SelectOption, SelectVariant } from '@/components/'
+import * as SelectRadix from '@radix-ui/react-select'
+import { clsx } from 'clsx'
+
+import s from '../Select.module.scss'
+
+export type SelectItemProps = {
+  className?: string
+  option?: SelectOption
+  value: string
+  variant: SelectVariant
+} & ComponentPropsWithoutRef<typeof SelectRadix.Root>
+
+export const SelectItem = forwardRef<ElementRef<typeof SelectRadix.Root>, SelectItemProps>(
+  (
+    { children, className, option = TypographyOption.Body1, variant, ...restProps },
+    ref
+  ): JSX.Element => {
+    const classNames = clsx(s[`${variant}Paddings`], s.selectItem, className)
+
+    return (
+      <SelectRadix.Item className={classNames} {...restProps} ref={ref}>
+        <SelectRadix.ItemText>
+          <Typography variant={option}>{children}</Typography>
+        </SelectRadix.ItemText>
+      </SelectRadix.Item>
+    )
+  }
+)
