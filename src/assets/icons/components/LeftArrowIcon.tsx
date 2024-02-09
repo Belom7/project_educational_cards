049be57bd68ad2448e5c0d93672ffe69c7132de6
Richard Leshik <richardleshik@gmail.com>
2024-02-09 22:13:30 +0300

    fix: edit LeftArrowIcon icon

--- src/assets/icons/components/LeftArrowIcon.tsx
+++ src/assets/icons/components/LeftArrowIcon.tsx
@@ -2,25 +2,19 @@ import { Ref, SVGProps, forwardRef, memo } from 'react'
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     fill={'none'}
-    height={16}
+    height={14}
     ref={ref}
+    viewBox={'0 0 14 16'}
     width={16}
     xmlns={'http://www.w3.org/2000/svg'}
     {...props}
   >
-    <g clipPath={'url(#a)'}>
-      <path
-        className={props.className}
-        d={
-          'M12.667 7.333H4.76l2.42-2.906a.667.667 0 1 0-1.027-.854l-3.333 4a.794.794 0 0 0-.06.1c0 .034 0 .054-.047.087a.667.667 0 0 0-.046.24c0 .082.016.164.046.24 0 .033 0 .053.047.087a.794.794 0 0 0 .06.1l3.333 4a.668.668 0 0 0 1.027-.854L4.76 8.667h7.907a.667.667 0 1 0 0-1.334Z'
-        }
-      />
-    </g>
-    <defs>
-      <clipPath id={'a'}>
-        <path d={'M0 0h16v16H0z'} fill={'#fff'} />
-      </clipPath>
-    </defs>
+    <path
+      d={
+        'M15 6H3.14l3.63-4.36A1 1 0 0 0 5.23.36l-5 6-.09.15c0 .05 0 .08-.07.13A1 1 0 0 0 0 7a1 1 0 0 0 .07.36c0 .05 0 .08.07.13l.09.15 5 6a1 1 0 0 0 1.54-1.28L3.14 8H15a1 1 0 1 0 0-2Z'
+      }
+      fill={'#fff'}
+    />
   </svg>
 )
 const ForwardRef = forwardRef(SvgComponent)
