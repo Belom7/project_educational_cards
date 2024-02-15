1a535430e86ce88abe3c5e955fdde5c64ae20afb
Richard Leshik <richardleshik@gmail.com>
2024-02-15 22:49:41 +0300

    feat: create cards slice

---
+++ src/features/decks/model/cards.slice.ts
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const initialState = {}
+
+export const cardsSlice = createSlice({
+  initialState,
+  name: 'cards',
+  reducers: {},
+})
+
+export const cardsActions = cardsSlice.actions
