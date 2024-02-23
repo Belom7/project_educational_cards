1b2cea9f7acdbd975e27539a664548029b055bf0
Richard Leshik <richardleshik@gmail.com>
2024-02-23 20:05:39 +0300

    feat: add search by input

--- src/pages/cards-page/CardsPage.tsx
+++ src/pages/cards-page/CardsPage.tsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { TypographyOption, useDebounce } from '@/common'
-import { BackToDecksLink, Button, Typography, WithoutTable } from '@/components'
+import { BackToDecksLink, Button, TextField, Typography, WithoutTable } from '@/components'
 import { AddCard, CardsTable, useGetCardsQuery, useGetDeckQuery, useMeQuery } from '@/features'
 import { useCardsOptions } from '@/features/decks/hooks/use-cards-options'
 
@@ -10,8 +10,16 @@ import s from './CardsPage.module.scss'
 
 export const CardsPage = () => {
   const { id = '' } = useParams<{ id: string }>()
-  const { currentPage, itemsPerPage, onChangeSort, onResetState, orderBy, question, sort } =
-    useCardsOptions()
+  const {
+    currentPage,
+    itemsPerPage,
+    onChangeSearchPhrase,
+    onChangeSort,
+    onResetState,
+    orderBy,
+    question,
+    sort,
+  } = useCardsOptions()
 
   const queryParams = {
     id,
@@ -39,54 +47,41 @@ export const CardsPage = () => {
       <div className={s.main}>
         <div className={s.wrapper}>
           <BackToDecksLink className={s.backToDecksList} title={'Back to Decks List'} />
-          {isCurrentUser &&
-            (isDeckNotEmpty ? (
-              <div>
-                <div className={s.nameDecks}>
-                  <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
+          {isDeckNotEmpty ? (
+            <div>
+              <div className={s.deckName}>
+                <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
+                {isCurrentUser ? (
                   <AddCard />
-                </div>
-                <CardsTable
-                  cards={deckData?.items || []}
-                  isCurrentUser
-                  onSort={onChangeSort}
-                  sort={sort}
-                />
-              </div>
-            ) : (
-              <div>
-                <div className={s.nameDecks}>
-                  <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
-                </div>
-                <div className={s.addingCardContentInfo}>
-                  <WithoutTable text={'This deck is empty. Click add new card to fill this deck'}>
-                    <AddCard />
-                  </WithoutTable>
-                </div>
-              </div>
-            ))}
-          {!isCurrentUser &&
-            (isDeckNotEmpty ? (
-              <div>
-                <div className={s.nameDecks}>
-                  <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
+                ) : (
                   <Button>
                     <Typography variant={TypographyOption.Subtitle2}>Learn to Deck</Typography>
                   </Button>
-                </div>
-                <CardsTable
-                  cards={deckData?.items || []}
-                  isCurrentUser={false}
-                  onSort={onChangeSort}
-                  sort={sort}
-                />
+                )}
               </div>
-            ) : (
-              <div>
-                <div className={s.nameDecks}>
-                  <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
-                </div>
-                <div className={s.addingCardContentInfo}>
+              <TextField
+                onValueChange={onChangeSearchPhrase}
+                placeholder={'Input search'}
+                value={question}
+              />
+              <CardsTable
+                cards={deckData?.items || []}
+                isCurrentUser={isCurrentUser}
+                onSort={onChangeSort}
+                sort={sort}
+              />
+            </div>
+          ) : (
+            <div>
+              <div className={s.deckName}>
+                <Typography variant={TypographyOption.H1}>{deck?.name}</Typography>
+              </div>
+              <div className={s.addingCardContentInfo}>
+                {isCurrentUser ? (
+                  <WithoutTable text={'This deck is empty. Click add new card to fill this deck'}>
+                    <AddCard />
+                  </WithoutTable>
+                ) : (
                   <WithoutTable text={'This deck is empty. Click Back to Decks List'}>
                     <BackToDecksLink
                       className={s.backToDecksList}
@@ -94,9 +89,10 @@ export const CardsPage = () => {
                       type={'primary'}
                     />
                   </WithoutTable>
-                </div>
+                )}
               </div>
-            ))}
+            </div>
+          )}
         </div>
       </div>
     </>
